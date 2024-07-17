83d3d379f1aeff93738cc8686224f9219331f16a
Zander Lewis <wolfthedev@gmail.com>
2024-07-17 12:03:16 -0400

    Update compiler.py (#1)
    Changes: 1. Add check to make sure the user uses a .markdownruntime file.  2. combine 'os.chdir("..")' and 'os.chdir("/code")'.

--- compiler/compiler.py
+++ compiler/compiler.py
@@ -1,12 +1,25 @@
 import os
+
+
 print("Please save your files in the code/ directory")
-name = input("Enter the file name: ")
-os.chdir("..")
-os.chdir("code")
+
+# Going with your method of asking for the name.
+# Another and better method would to use arguments
+# when running. For example: 'python3 compiler.py hello.markdownruntime'.
+while True:
+    name = input("Enter the file name: ")
+    if name.endswith(".markdownruntime"):
+        break
+
+# Save a few keystrokes by combining
+os.chdir("../code")
+
 if os.name == "nt":
     os.system("cls")
 else:
     os.system("clear")
+
+
 with open(name, "r") as f:
     line = f.readline()
     if line.startswith("#"):
